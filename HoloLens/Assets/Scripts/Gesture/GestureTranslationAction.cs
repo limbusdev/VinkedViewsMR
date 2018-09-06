9f2e497e4d8fb1d000c2d9af210c89841b52ff84
Georg Eckert <eckert.georg@gmx.de>
2018-09-06 12:35:53 +0200

    adds Translation Gesture Script with Constraint possibilities

--- HoloLens/Assets/Scripts/Gesture/GestureTranslationAction.cs
+++ HoloLens/Assets/Scripts/Gesture/GestureTranslationAction.cs
@@ -1,37 +1,31 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-using HoloToolkit.Unity.InputModule;
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.InputModule.Utilities.Interactions;
 using UnityEngine;
 
-namespace Academy
+namespace Gestures
 {
-    /// <summary>
-    /// GestureAction performs custom actions based on 
-    /// which gesture is being performed.
-    /// </summary>
-    public class GestureTranslationAction : MonoBehaviour, INavigationHandler, IManipulationHandler
+    public enum AxisAndPlaneConstraint
     {
+        NONE, X_AXIS_ONLY, Y_AXIS_ONLY, Z_AXIS_ONLY, XY_PLANE_ONLY, XZ_PLANE_ONLY, YZ_PLANE_ONLY
+    }
+
+    public class GestureTranslationAction : MonoBehaviour, IManipulationHandler
+    {
+        [SerializeField]
+        [Tooltip("Transform that will be dragged. Default is the components GameObject.")]
+        private Transform hostTransform = null;
+
+        [SerializeField]
+        [Tooltip("Constrain translation along an axis.")]
+        private AxisAndPlaneConstraint translationConstraint = AxisAndPlaneConstraint.NONE;
+
         private Vector3 manipulationOriginalPosition = Vector3.zero;
 
-        void INavigationHandler.OnNavigationStarted(NavigationEventData eventData)
-        {
-            InputManager.Instance.PushModalInputHandler(gameObject);
-        }
 
-        void INavigationHandler.OnNavigationUpdated(NavigationEventData eventData)
-        {
 
-        }
-
-        void INavigationHandler.OnNavigationCompleted(NavigationEventData eventData)
+        void Awake()
         {
-            InputManager.Instance.PopModalInputHandler();
-        }
-
-        void INavigationHandler.OnNavigationCanceled(NavigationEventData eventData)
-        {
-            InputManager.Instance.PopModalInputHandler();
+            if(hostTransform == null) hostTransform = transform;
         }
 
         void IManipulationHandler.OnManipulationStarted(ManipulationEventData eventData)
@@ -39,13 +33,44 @@ namespace Academy
 
                 InputManager.Instance.PushModalInputHandler(gameObject);
 
-                manipulationOriginalPosition = transform.position;
+                manipulationOriginalPosition = hostTransform.position;
 
         }
 
         void IManipulationHandler.OnManipulationUpdated(ManipulationEventData eventData)
         {
-                transform.position = manipulationOriginalPosition + eventData.CumulativeDelta;
+            Vector3 positionUpdate = eventData.CumulativeDelta;
+
+            switch(translationConstraint)
+            {
+                case AxisAndPlaneConstraint.X_AXIS_ONLY:
+                    positionUpdate.y = 0;
+                    positionUpdate.z = 0;
+                    break;
+                case AxisAndPlaneConstraint.Y_AXIS_ONLY:
+                    positionUpdate.x = 0;
+                    positionUpdate.z = 0;
+                    break;
+                case AxisAndPlaneConstraint.Z_AXIS_ONLY:
+                    positionUpdate.z = positionUpdate.x; // Map X-Component of Gesture to z-Position
+                    positionUpdate.x = 0;
+                    positionUpdate.y = 0;
+                    break;
+                case AxisAndPlaneConstraint.XY_PLANE_ONLY:
+                    positionUpdate.z = 0;
+                    break;
+                case AxisAndPlaneConstraint.XZ_PLANE_ONLY:
+                    positionUpdate.y = 0;
+                    break;
+                case AxisAndPlaneConstraint.YZ_PLANE_ONLY:
+                    positionUpdate.z = positionUpdate.x;
+                    positionUpdate.x = 0;
+                    break;
+                default: // case NONE:
+                    break;
+            }
+
+            hostTransform.position = manipulationOriginalPosition + positionUpdate;
         }
 
         void IManipulationHandler.OnManipulationCompleted(ManipulationEventData eventData)

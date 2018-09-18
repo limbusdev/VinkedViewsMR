0cebc34405bcc3e70c3f3dede0a96cd1aca3b7d5
Georg Eckert <eckert.georg@gmx.de>
2018-09-18 12:47:43 +0200

    adds some UI stuff

---
+++ HoloLens/Assets/UI/Scripts/VisFactoryInteractionReceiver.cs
@@ -0,0 +1,41 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.Receivers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.Buttons;
+
+public class VisFactoryInteractionReceiver : InteractionReceiver
+{
+    public GameObject HoloButtonPrefab;
+    public GameObject screenAnchor;
+    public DataProvider dataProvider;
+
+    protected override void InputDown(GameObject obj, InputEventData eventData)
+    {
+        Debug.Log(obj.name + " : InputDown");
+
+        switch(obj.name)
+        {
+            case "ButtonDataSets":
+                interactables[0].SetActive(false);
+                interactables[1].SetActive(true);
+                interactables[2].SetActive(true);
+                interactables[3].SetActive(true);
+                interactables[4].SetActive(true);
+                break;
+            case "Back":
+                interactables[0].SetActive(true);
+                interactables[1].SetActive(false);
+                interactables[2].SetActive(false);
+                interactables[3].SetActive(false);
+                interactables[4].SetActive(false);
+                break;
+            
+
+            default:
+                break;
+        }
+    }
+}

c98140ea619064a566f549b8d3d11d34b509ed64
Georg Eckert <eckert.georg@gmx.de>
2019-01-14 20:26:30 +0100

    adds GPLv3 notice to all code files of Vinked Views

---
+++ CREDITS.md
@@ -0,0 +1,4 @@
+# Credits
+
+* [HoloToolkit](https://github.com/Microsoft/MixedRealityToolkit-Unity) / Mixed Reality Toolkit by Microsoft 2018 (License: MIT)
+* [Unity](https://unity3d.com/de)
--- HoloLens/Assets/Plugins/CurvedLine/CurvedLineRenderer.cs
+++ HoloLens/Assets/Plugins/CurvedLine/CurvedLineRenderer.cs
@@ -1,6 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 [RequireComponent( typeof(LineRenderer) )]
 public class CurvedLineRenderer : MonoBehaviour 

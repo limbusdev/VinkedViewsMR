401465ef692e608692b7e5ac526c6e165539d5d1
Georg Eckert <eckert.georg@gmx.de>
2019-01-14 19:16:30 +0100

    applies icons in visualization factory

--- HoloLens/Assets/UI/Scripts/VisFactoryInteractionReceiver.cs
+++ HoloLens/Assets/UI/Scripts/VisFactoryInteractionReceiver.cs
@@ -32,43 +32,29 @@ public class VisFactoryInteractionReceiver : InteractionReceiver
         {
             case "ButtonDataSets":
                 DeactivateAllInteractibles();
-                ActivateInteractables(new int[] { 1, 2, 4, 10 });
+                ActivateInteractables(new int[] { 1,2,3,4,5 });
                 break;
             case "Back":
                 DeactivateAllInteractibles();
-                ActivateInteractables(new int[] { 0, 10 });
+                ActivateInteractables(new int[] { 0, 5 });
                 break;
             case "ButtonDataSet1":
                 currentlyChosenDataBase = 0;
                 DeactivateAllInteractibles();
-                ActivateInteractables(new int[] { 5, 6, 7, 8, 9, 10 });
+                ActivateInteractables(new int[] { 1,4,5 });
+                SetupGalery(currentlyChosenDataBase);
                 break;
             case "ButtonDataSet2":
                 currentlyChosenDataBase = 1;
                 DeactivateAllInteractibles();
-                ActivateInteractables(new int[] { 5, 6, 8, 9, 10 });
+                ActivateInteractables(new int[] { 2,4,5 });
+                SetupGalery(currentlyChosenDataBase);
                 break;
             case "ButtonDataSet3":
                 currentlyChosenDataBase = 2;
                 DeactivateAllInteractibles();
-                ActivateInteractables(new int[] { 5, 6, 7, 8, 9, 10 });
-                break;
-            case "Button1D":
-                SetupGalery(currentlyChosenDataBase, 1);
-                break;
-            case "Button2D":
-                SetupGalery(currentlyChosenDataBase, 2);
-                break;
-            case "Button3D":
-                SetupGalery(currentlyChosenDataBase, 3);
-                break;
-            case "ButtonND":
-                SetupGalery(currentlyChosenDataBase, 4);
-                break;
-            case "Back2":
-                DeactivateAllInteractibles();
-                ActivateInteractables(new int[] { 1,2,3,4, 10 });
-                ClearGallery();
+                ActivateInteractables(new int[] { 3,4,5 });
+                SetupGalery(currentlyChosenDataBase);
                 break;
             case "CubeIconVariable":
                 HideAllIconSubButtons();
@@ -168,16 +154,9 @@ public class VisFactoryInteractionReceiver : InteractionReceiver
     /// </summary>
     /// <param name="dataBaseID"></param>
     /// <param name="dimension"></param>
-    private void SetupGalery(int dataBaseID, int dimension)
+    private void SetupGalery(int dataBaseID)
     {
-        switch(dimension)
-        {
-            case 3: Setup3DGalery(dataBaseID); break;
-            case 2: Setup2DGalery(dataBaseID); break;
-            case 1: Setup1DGalery(dataBaseID); break;
-            default: SetupNDGalery(dataBaseID); break;
-        }
-
+        Setup1DGalery(dataBaseID);
         ObjectCollection.GetComponent<ObjectCollection>().UpdateCollection();
     }
 
@@ -201,190 +180,13 @@ public class VisFactoryInteractionReceiver : InteractionReceiver
             interactables.Add(Create1DIconAndInsert(m.name, m.type));
         }
     }
-    
-    /// <summary>
-    /// Generates a galery by combining all available attributes to 2D pairs.
-    /// </summary>
-    /// <param name="dbID"></param>
-    private void Setup2DGalery(int dbID)
-    {
-        DataSet ds = Services.DataBase().dataSets[dbID];
-
-        var ms = new List<AttributeStats>();
-        ms.AddRange(ds.nominalStatistics.Values);
-        ms.AddRange(ds.ordinalStatistics.Values);
-        ms.AddRange(ds.intervalStatistics.Values);
-        ms.AddRange(ds.rationalStatistics.Values);
-
-        var keys = new List<IconKey2D>();
-        string debug = "";
-
-        foreach(var m1 in ms)
-        {
-            foreach(var m2 in ms)
-            {
-                var key = new IconKey2D(m1, m2);
-                if(!m1.name.Equals(m2.name))
-                {
-                    keys.Add(key);
-                    interactables.Add(Create2DIconAndInsert(m1.name, m2.name, m1.type, m2.type));
-                    debug += m1.name + " x " + m2.name + "\n";
-                }
-            }
-        }
-
-        
-
-        foreach(var m in ms)
-        {
-            
-        }
-
-        Debug.Log(debug);
-    }
-
-    /// <summary>
-    /// Generates a galery by combining all available attributes to 3D groups.
-    /// </summary>
-    /// <param name="dbID"></param>
-    private void Setup3DGalery(int dbID)
-    {
-        DataSet ds = Services.DataBase().dataSets[dbID];
-        
-        var ms = new List<AttributeStats>();
-        ms.AddRange(ds.nominalStatistics.Values);
-        ms.AddRange(ds.ordinalStatistics.Values);
-        ms.AddRange(ds.intervalStatistics.Values);
-        ms.AddRange(ds.rationalStatistics.Values);
-
-        var keys = new List<IconKey>();
-
-
-        foreach(var m1 in ms)
-        {
-            foreach(var m2 in ms)
-            {
-                foreach(var m3 in ms)
-                {
-                    var key = new IconKey(m1, m2, m3);
-                    if(!keys.Contains(key) && !(m1.Equals(m2) || m2.Equals(m3) || m3.Equals(m1)))
-                    {
-                        keys.Add(key);
-                        interactables.Add(Create3DIconAndInsert(m1.name, m2.name, m3.name, m1.type, m2.type, m3.type));
-                    }
-                }
-            }
-        }
-    }
-
-    /// <summary>
-    /// Generates a galery by grouping attributes all together or by level of measurement.
-    /// </summary>
-    /// <param name="dbID"></param>
-    private void SetupNDGalery(int dbID)
-    {
-        DataSet ds = Services.DataBase().dataSets[dbID];
-
-        // 1 Icon for all variables
-        var allVars = new List<string>();
-        var allLoms = new List<LoM>();
-        foreach(string key in ds.nominalStatistics.Keys)
-        {
-            allVars.Add(key);
-            allLoms.Add(ds.nominalStatistics[key].type);
-        }
-        foreach(string key in ds.ordinalStatistics.Keys)
-        {
-            allVars.Add(key);
-            allLoms.Add(ds.ordinalStatistics[key].type);
-        }
-        foreach(string key in ds.intervalStatistics.Keys)
-        {
-            allVars.Add(key);
-            allLoms.Add(ds.intervalStatistics[key].type);
-        }
-        foreach(string key in ds.rationalStatistics.Keys)
-        {
-            allVars.Add(key);
-            allLoms.Add(ds.rationalStatistics[key].type);
-        }
-
-        if(allVars.Count > 0)
-            interactables.Add(CreateNDIconAndInsert(allVars.ToArray(), allLoms.ToArray(), "All Attr."));
-
-        // 1 Icon for all nominal variables
-        allVars.Clear();
-        allLoms.Clear();
-
-        foreach(string key in ds.nominalStatistics.Keys)
-        {
-            allVars.Add(key);
-            allLoms.Add(ds.nominalStatistics[key].type);
-        }
-
-        if(allVars.Count > 0)
-            interactables.Add(CreateNDIconAndInsert(allVars.ToArray(), allLoms.ToArray(), "All Nom."));
-
-        // 1 Icon for all ordinal variables
-        allVars.Clear();
-        allLoms.Clear();
-
-        foreach(string key in ds.ordinalStatistics.Keys)
-        {
-            allVars.Add(key);
-            allLoms.Add(ds.ordinalStatistics[key].type);
-        }
-
-        if(allVars.Count > 0)
-            interactables.Add(CreateNDIconAndInsert(allVars.ToArray(), allLoms.ToArray(), "All Ord."));
-
-        // 1 Icon for all interval variables
-        allVars.Clear();
-        allLoms.Clear();
-
-        foreach(string key in ds.intervalStatistics.Keys)
-        {
-            allVars.Add(key);
-            allLoms.Add(ds.intervalStatistics[key].type);
-        }
-
-        if(allVars.Count > 0)
-            interactables.Add(CreateNDIconAndInsert(allVars.ToArray(), allLoms.ToArray(), "All Intv."));
-
-        // 1 Icon for all ratio variables
-        allVars.Clear();
-        allLoms.Clear();
-
-        foreach(string key in ds.rationalStatistics.Keys)
-        {
-            allVars.Add(key);
-            allLoms.Add(ds.rationalStatistics[key].type);
-        }
-
-        if(allVars.Count > 0)
-            interactables.Add(CreateNDIconAndInsert(allVars.ToArray(), allLoms.ToArray(), "All Ratio"));
-
-    }
+   
 
     private GameObject Create1DIconAndInsert(string name, LoM lom)
     {
        return CreateIconAndInsertInCollection(new string[] { name }, new LoM[] { lom });
     }
-
-    private GameObject Create2DIconAndInsert(string name1, string name2, LoM lom1, LoM lom2)
-    {
-        return CreateIconAndInsertInCollection(new string[] { name1, name2 }, new LoM[] { lom1, lom2 });
-    }
-
-    private GameObject Create3DIconAndInsert(string name1, string name2, string name3, LoM lom1, LoM lom2, LoM lom3)
-    {
-        return CreateIconAndInsertInCollection(new string[] { name1, name2, name3 }, new LoM[] { lom1, lom2, lom3 });
-    }
-
-    private GameObject CreateNDIconAndInsert(string[] names, LoM[] loms, string name)
-    {
-        return CreateMultiIconAndInsertInCollection(names, loms, name);
-    }
+    
 
     private GameObject CreateIconAndInsertInCollection(string[] names, LoM[] loms)
     {
@@ -436,6 +238,41 @@ public class VisFactoryInteractionReceiver : InteractionReceiver
                 var button = Instantiate(HoloButtonPrefab);
 
                 button.AddComponent<VisTypeButton>().visType = visType;
+
+                var cb = button.GetComponent<CompoundButtonIcon>();
+                switch(visType)
+                {
+                    case VisType.Histogram2D:
+                        cb.IconName = "Icon2D";
+                        break;
+                    case VisType.Histogram3D:
+                        cb.IconName = "Icon3D";
+                        break;
+                    case VisType.HistogramHeatmap3D:
+                        cb.IconName = "Icon3D";
+                        break;
+                    case VisType.LineChart2D:
+                        cb.IconName = "Icon2D";
+                        break;
+                    case VisType.PCP2D:
+                        cb.IconName = "IconPCP";
+                        break;
+                    case VisType.PCP3D:
+                        cb.IconName = "IconPCP";
+                        break;
+                    case VisType.ScatterPlot2D:
+                        cb.IconName = "Icon2D";
+                        break;
+                    case VisType.ScatterPlot3D:
+                        cb.IconName = "Icon3D";
+                        break;
+                    case VisType.SingleAxis3D:
+                        cb.IconName = "Icon1D";
+                        break;
+                    default: cb.IconName = "Anchor"; break;
+                }
+
+                
 
                 button.name = visType.ToString();
                 button.GetComponent<CompoundButtonText>().Text = visType.ToString();
